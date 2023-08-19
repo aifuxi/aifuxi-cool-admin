1a49e6aaca03ed37b9326a13736b612c23bc450b
aifuxi <aifuxi.js@gmail.com>
2023-08-20 00:47:53 +0800

    feat✨: 菜单布局完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,36 @@ import { ROUTE_PATH } from '@/constants/path';
 import Home from '@/pages/home/home';
 import Login from '@/pages/login/login';
 
+import MainLayout from './components/layout/main-layout';
+import Tag from './pages/tag/tag';
+
 const router = createBrowserRouter([
-  {
-    path: ROUTE_PATH.HOME,
-    element: <Home />,
-  },
   {
     path: ROUTE_PATH.LOGIN,
     element: <Login />,
   },
+  {
+    path: ROUTE_PATH.HOME,
+    element: <MainLayout />,
+    children: [
+      {
+        path: ROUTE_PATH.HOME,
+        element: <Home />,
+      },
+      {
+        path: ROUTE_PATH.TAG,
+        element: <Tag />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout/main-layout.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+import { Avatar, Layout, Menu, Typography } from '@arco-design/web-react';
+import { IconCalendar, IconHome } from '@arco-design/web-react/icon';
+
+import { ROUTE_PATH } from '@/constants/path';
+
+const MenuItem = Menu.Item;
+// const SubMenu = Menu.SubMenu;
+const Sider = Layout.Sider;
+const Header = Layout.Header;
+const Footer = Layout.Footer;
+const Content = Layout.Content;
+
+const MainLayout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSelectedKeys([location.pathname]);
+  }, [location]);
+
+  return (
+    <Layout className="h-screen w-screen">
+      <Sider collapsed={false} collapsible trigger={null} breakpoint="xl">
+        <Typography.Title heading={4} className="text-center">
+          F西后台管理系统
+        </Typography.Title>
+        <Menu
+          selectedKeys={selectedKeys}
+          onClickMenuItem={(key) => {
+            setSelectedKeys([key]);
+            navigate(key);
+          }}
+          style={{ width: '100%' }}
+        >
+          <MenuItem key={ROUTE_PATH.HOME}>
+            <IconHome />
+            Home
+          </MenuItem>
+          <MenuItem key={ROUTE_PATH.TAG}>
+            <IconCalendar />
+            文章标签
+          </MenuItem>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header className="h-[60px] flex items-center justify-end px-6">
+          <Avatar>
+            <img
+              alt="avatar"
+              src="https://aifuxi.oss-cn-shanghai.aliyuncs.com/self/avatar.jpeg"
+            />
+          </Avatar>
+        </Header>
+        <Layout className="p-6">
+          <Content>
+            <Outlet />
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
+<h2 className=""></h2>;
+
+export default MainLayout;
